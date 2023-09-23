4799b94baa419ead97cac08bd1065b042ccc2c80
dskorolev <dskorolev@avito.ru>
2023-09-23 18:51:30 +0300

    add request to backend

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,7 @@
 console.log("Open popup page")
 
+const URL = "http://127.0.0.1:8000?"
+
 function formatRow(inner) {
     return `
     <tr>
@@ -18,9 +20,36 @@ function formatColumns(title, description, rating) {
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     let newInnerHTML = formatRow(formatColumns("Название", "Описание", "Рейтинг"))
-    console.log("background job selected text 1: " + request.data);
-    newInnerHTML += formatRow(formatColumns("эээ ну это ммм эээ", request.data, 9))
-    console.log("background job selected text 2: " + request.data);
-    document.querySelector(".results-table").innerHTML = newInnerHTML
+    let similarProblems
+    console.log("selected text: " + request.data);
+
+    fetch(URL + "text=" + request.data)
+        .then(response => {
+            // Check if the response status is OK (status code 200)
+            if (response.status === 200) {
+                // Parse the response body as JSON
+                return response.json();
+            } else {
+                // Handle the error if the status code is not 200
+                throw new Error('Failed to fetch data');
+            }
+        })
+        .then(data => {
+            // Log the data to the console
+            console.log("response: " + data);
+            similarProblems = data.response
+
+            for (const similarProblem of similarProblems) {
+                newInnerHTML += formatRow(formatColumns(similarProblem["name"], similarProblem["description"], similarProblem["rating"]))
+            }
+            console.log("newInnerHTML: " + newInnerHTML);
+            document.querySelector(".results-table").innerHTML = newInnerHTML
+        })
+        .catch(error => {
+            // Log any errors to the console
+            console.error(error);
+        });
+
     sendResponse()
+    console.log("—————————————————————————————————————");
 });
